fcea340c760681484bf7662d8c10b89616b8f822
Mohammed Shahriyaar <mohammedshahriyaar@gmail.com>
2024-05-21 01:16:26 +0530

    admin added tailwind added

--- shahaurDjango/shahaurDjango/settings.py
+++ shahaurDjango/shahaurDjango/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shah',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS=['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'shahaurDjango.urls'
@@ -123,3 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#nodejs path
+NPM_BIN_PATH = r"D:\nodejs\npm.cmd"
--- shahaurDjango/shahaurDjango/urls.py
+++ shahaurDjango/shahaurDjango/urls.py
@@ -23,5 +23,9 @@ urlpatterns = [
     path('' ,views.home,name='home'),
     path('about/' ,views.about,name='about'),
     path('contact/' ,views.contact,name='contact'),
-    path('shah/' ,include('shah.urls'))
+    path('shah/' ,include('shah.urls')),
+
+
+    
+    path("__reload__/", include("django_browser_reload.urls")),
 ]

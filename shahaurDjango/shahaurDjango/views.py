b5c312341a3daf853d17a55d6afcdc5d391e6b99
Mohammed Shahriyaar <mohammedshahriyaar@gmail.com>
2024-05-21 00:09:02 +0530

    using templates everywhere

--- shahaurDjango/shahaurDjango/views.py
+++ shahaurDjango/shahaurDjango/views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("HEllo,sher Welcome to django homepage")
+    # return HttpResponse("HEllo,sher Welcome to django homepage")
 
     # return render(request,'index.html')
-    # return render(request,'website/index.html')
+    return render(request,'website/index.html')
 
 
 def contact(request):
